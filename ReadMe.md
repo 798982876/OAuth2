e84ce8d988e080014dbbcc6879df72ac2e898407
zhang jie <798982876@qq.com>
2021-12-21 15:10:09 +0800

    post √

--- ReadMe.md
+++ ReadMe.md
@@ -1,2 +1,4 @@
 ## 实现步骤
 1. 实现GET
+2. POST 需要加上`xhr.setRequestHeader("Content-Type","application/json");";`
+不然接收到的的body为空
--- javascripts/oauth.js
+++ javascripts/oauth.js
@@ -1,12 +1,12 @@
 let  xhr = new XMLHttpRequest();
 xhr.open( 'POST', 'http://localhost:3000/data' , true );
+xhr.setRequestHeader("Content-Type","application/json");
 xhr.onreadystatechange = function () {
   if (xhr.readyState !== 4 || xhr.status !== 200) {
     return;
   }
   console.log(xhr.responseText);
 };
-const formData = new FormData();
 const pgfun = 'oauth2.svr_generate_refresh_access_token';
 const content = {
   "user_name":"u4",
@@ -14,8 +14,12 @@ const content = {
   "client_id":"a2c0e4ea-31d3-4657-86e2-ff8baa720ebb",
   "client_secret":"e4e0f69a2dab2d37e4f3f3e3e4838cf2"
 };
-formData.append('pgfun',pgfun);
-formData.append('content',content);
+const body = {
+  pgfun:pgfun,
+  content:JSON.stringify(content)
+}
+// formData.append('pgfun',pgfun);
+// formData.append('content',JSON.stringify(content));
 
-xhr.send(formData);
+xhr.send(JSON.stringify(body));
 
